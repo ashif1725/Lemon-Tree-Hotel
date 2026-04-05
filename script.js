7e7c231018581bdc676f719d80a5841adabff7dd
ashif1725 <ashif9720@gmail.com>
2026-04-06 03:10:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-
 let menu = document.querySelector(".menu-btn");
 let side = document.querySelector(".side-menu");
 let close = document.querySelector(".close-btn");
 
-if(menu){
-  menu.onclick = () => side.classList.add("active");
-}
+menu.onclick = () => side.classList.add("active");
+close.onclick = () => side.classList.remove("active");
 
-if(close){
-  close.onclick = () => side.classList.remove("active");
-}
-
-// Booking Form
+// Booking
 let bookBtn = document.querySelector(".book-btn");
 let form = document.querySelector(".booking-form");
 let closeForm = document.querySelector(".close-form");
 
-if(bookBtn){
-  bookBtn.onclick = () => form.classList.add("active");
-}
-
-if(closeForm){
-  closeForm.onclick = () => form.classList.remove("active");
-}
+bookBtn.onclick = () => form.classList.add("active");
+closeForm.onclick = () => form.classList.remove("active");
